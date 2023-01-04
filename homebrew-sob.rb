f3175188a8156d03fb66e10006c2ddf0cffde3c8
AgentofAsgard <100227951+AgentofAsgard@users.noreply.github.com>
2023-01-04 15:12:15 -0800

    Update homebrew-sob.rb

--- homebrew-sob.rb
+++ homebrew-sob.rb
@@ -20,7 +20,7 @@ class HomebrewSob < Formula
           -X github.com/AgentofAsgard/sg/internal/version.timestamp=$(date +%s)
           -X github.com/AgentofAsgard/sg/internal/conf/deploy.forceType=single-program
         ]
-        system "go", "build", *std_go_args(output: bin/"sob", ldflags: ldflags), "./enterprise/cmd/sourcegraph"
+        system "go", "build", *std_go_args(output: bin/"sob", ldflags: ldflags), "github.com/AgentofAsgard/sg/enterprise/cmd/sourcegraph"
       end
 
 end
