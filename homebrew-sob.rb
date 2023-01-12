a7cc67d3a69fb233eb4abc068bfabdd3ce562505
AgentofAsgard <100227951+AgentofAsgard@users.noreply.github.com>
2023-01-11 22:54:08 -0800

    Update homebrew-sob.rb

--- homebrew-sob.rb
+++ homebrew-sob.rb
@@ -8,9 +8,6 @@ class HomebrewSob < Formula
         ENV["ENTERPRISE"] = "1"
         ENV["DEV_WEB_BUILDER"] = "esbuild yarn run build-web"
         ldflags = %W[
-          -s -w
-          -X github.com/AgentofAsgard/sg/internal/version.version=${VERSION-0.0.0+dev}
-          -X github.com/AgentofAsgard/sg/internal/version.timestamp=$(date +%s)
           -X github.com/AgentofAsgard/sg/internal/conf/deploy.forceType=single-program
         ]
         system "go", "build", *std_go_args(output: bin/"sgapp", ldflags: ldflags), "#{Formula["homebrew-sob"].libexec}/enterprise/cmd/sourcegraph"
