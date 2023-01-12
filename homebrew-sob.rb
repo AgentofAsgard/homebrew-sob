45fb0868f11f83cbc449c92c957bb7e68ef6b34c
AgentofAsgard <100227951+AgentofAsgard@users.noreply.github.com>
2023-01-11 23:01:54 -0800

    Update homebrew-sob.rb

--- homebrew-sob.rb
+++ homebrew-sob.rb
@@ -5,11 +5,6 @@ class HomebrewSob < Formula
     url "https://github.com/AgentofAsgard/sg.git", :branch => "new-main"
 
     def install
-        ENV["ENTERPRISE"] = "1"
-        ENV["DEV_WEB_BUILDER"] = "esbuild yarn run build-web"
-        ldflags = %W[
-          -X github.com/AgentofAsgard/sg/internal/conf/deploy.forceType=single-program
-        ]
-        system "go", "build", *std_go_args(output: bin/"sgapp", ldflags: ldflags), "#{Formula["homebrew-sob"].libexec}/enterprise/cmd/sourcegraph"
+        bin.install "enterprise/cmd/sourcegraph/build.sh"
     end
 end
